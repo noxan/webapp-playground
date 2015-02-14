8640c81769462573e4446645635abc289062bc09
noxan <noxan@byteweaver.net>
2015-02-14 13:34:16 +0000

    setup browserify with vinyl-transform and plumber

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,25 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
+var transform = require('vinyl-transform');
+var browserify = require('browserify');
 
-gulp.task('default', []);
+
+var browserified = transform(function(filename) {
+  var b = browserify(filename);
+  return b.bundle();
+});
+
+var config = {
+  scripts: ['./src/app.js'],
+  dist: './dist/'
+};
+
+gulp.task('scripts', function () {
+  gulp.src(config.scripts)
+    .pipe(plugins.plumber())
+    .pipe(browserified)
+    .pipe(gulp.dest(config.dist));
+});
+
+
+gulp.task('default', ['scripts']);
