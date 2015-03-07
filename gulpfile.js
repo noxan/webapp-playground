6c4c030a8bb5085782b85b99fd08952bc454581f
noxan <noxan@byteweaver.net>
2015-03-07 11:51:26 +0100

    add jslint simple with jshint reporter and add suiting task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -48,3 +48,15 @@ gulp.task('serve', ['build'], function () {
 
 
 gulp.task('default', ['serve']);
+
+
+gulp.task('jslint', function () {
+  return gulp.src(['./src/**/*.js', 'gulpfile.js'])
+    .pipe(plugins.plumber())
+    .pipe(plugins.jslintSimple.run({
+      indent: 2
+    }))
+    .pipe(plugins.jslintSimple.report({
+      reporter: require('jshint-stylish').reporter
+    }));
+});
