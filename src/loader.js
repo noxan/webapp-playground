306876d79a76eaabeb4617929ea900ff20187031
noxan <noxan@byteweaver.net>
2015-03-07 11:55:23 +0100

    fix jslint warnings for loader.js

--- src/loader.js
+++ src/loader.js
@@ -1,10 +1,15 @@
-var $loader = require('../dist/vendor/angular-loader/angular-loader.js');
-var $script = require('../dist/vendor/script.js/dist/script.min.js');
+/*global require: true, angular: true, document: true*/
+(function () {
+  'use strict';
 
-$script([
-  'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.13/angular.min.js',
-  'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.13/angular-route.min.js',
-  'app.js'
-], function () {
-  angular.bootstrap(document, ['edokiz']);
-});
+  require('../dist/vendor/angular-loader/angular-loader.js');
+  var $script = require('../dist/vendor/script.js/dist/script.min.js');
+
+  $script([
+    'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.13/angular.min.js',
+    'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.13/angular-route.min.js',
+    'app.js'
+  ], function () {
+    angular.bootstrap(document, ['edokiz']);
+  });
+}());
