1c101d3ec19984ea516929050112d3ce9eb83a57
noxan <noxan@byteweaver.net>
2015-03-07 14:21:43 +0100

    add spin.js loading animation until angular gets bootstrapped

--- src/loader.js
+++ src/loader.js
@@ -2,14 +2,18 @@
 (function () {
   'use strict';
 
-  require('../dist/vendor/angular-loader/angular-loader.js');
-  var $script = require('../dist/vendor/script.js/dist/script.min.js');
+  var Spinner = require('../dist/vendor/spin.js/spin.js');
+  var spinner = new Spinner().spin(document.body);
 
+  require('../dist/vendor/angular-loader/angular-loader.js');
+
+  var $script = require('../dist/vendor/script.js/dist/script.min.js');
   $script([
     'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.13/angular.min.js',
     'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.13/angular-route.min.js',
     'app.js'
   ], function () {
+    spinner.stop();
     angular.bootstrap(document, ['edokiz']);
   });
 }());
