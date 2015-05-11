f1eec2f35708d15cc81bacde16d2d0d1f44b9275
noxan <noxan@byteweaver.org>
2015-05-11 14:09:08 +0200

    merge scripts-bundle and scripts gulp files

--- gulpfile.js/scripts.js
+++ gulpfile.js/scripts.js
@@ -1,8 +1,32 @@
+var browserify = require('browserify');
 var es = require('event-stream');
-var bundle = require('./scripts-bundle');
+var path = require('path');
+var transform = require('vinyl-transform');
+var watchify = require('watchify');
 
 
 var scriptsTask = function (gulp, plugins, config) {
+  var bundler = function (options) {
+    return transform(function (filename) {
+      var b = browserify(filename, options);
+      if (config.watch) {
+        b = watchify(b);
+        b.on('update', bundle.bind(null, filename));
+      }
+      b.on('bundle', function () {
+        plugins.util.log("Script '" + plugins.util.colors.cyan(path.basename(filename)) + "' was browserified.");
+      });
+      return b.bundle();
+    });
+  };
+
+  var bundle = function (filename) {
+    return gulp.src(filename)
+      .pipe(plugins.plumber())
+      .pipe(bundler(watchify.args))
+      .pipe(gulp.dest(config.dist));
+  };
+
   return function () {
     return es.concat.apply(null, config.scripts.map(function (filename) {
       return bundle(filename);
