c32a4a7ca63b51dcda0b94657fe3620ac4ef0c3b
noxan <noxan@byteweaver.org>
2015-05-11 14:04:09 +0200

    move jslint task to own file

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -31,13 +31,4 @@ gulp.task('watch', ['set-watch', 'build'], function () {
 gulp.task('default', ['watch', 'serve']);
 
 
-gulp.task('jslint', function () {
-  return gulp.src(['src/**/*.js', 'gulpfile.js/**/*.js'])
-    .pipe(plugins.plumber())
-    .pipe(plugins.jslintSimple.run({
-      indent: 2
-    }))
-    .pipe(plugins.jslintSimple.report({
-      reporter: require('jshint-stylish').reporter
-    }));
-});
+gulp.task('jslint', require('./jslint')(gulp, plugins, config));
