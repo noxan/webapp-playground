68b1eb3c37e23dc9bb19da23ce651503ff2a7065
noxan <noxan@byteweaver.org>
2015-05-12 09:03:48 +0200

    add gulp-autoprefixer for stylus files

--- gulpfile.js/styles.js
+++ gulpfile.js/styles.js
@@ -6,6 +6,7 @@ module.exports = function (gulp, plugins, config) {
     return gulp.src(config.styles)
       .pipe(plugins.plumber())
       .pipe(plugins.stylus())
+      .pipe(plugins.autoprefixer())
       .pipe(plugins.minifyCss())
       .pipe(plugins.size({showFiles: true}))
       .pipe(gulp.dest(config.dist))
